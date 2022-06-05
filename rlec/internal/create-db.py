283181b681bb28c32749e73200a3c34976646bbc
rafie <rafi@redislabs.com>
2022-06-05 13:54:29 +0300

    internal/create-db.py: use yaml instead of json

--- rlec/internal/create-db.py
+++ rlec/internal/create-db.py
@@ -11,12 +11,18 @@ import psutil
 import shutil
 import argparse
 import requests
+import re
+import inspect
+
+from cnm import cnm_config
 
 from rlec_auto import *
 
 sys.path.insert(0, "/opt/readies")
 import paella
 BB()
+
+rlec_version = paella.Version(cnm_config.cnm_version)
 
 #----------------------------------------------------------------------------------------------
 
@@ -58,6 +64,19 @@ def memunit_to_bytes(mem):
     except:
         return None
 
+def remove_comments(s):
+    return "\n".join(list(map(lambda x: re.sub(r'^#.*$', '', x), s.split("\n"))))
+
+def join_blank_lines(s):
+    return re.sub(r'\s+\n', '\n', s)
+
+def meld(template):
+    if template[0] == '\n':
+        template = template[1:-1]
+    t = remove_comments(template)
+    s = jinja2.Template(t).render(inspect.currentframe().f_back.f_locals)
+    return join_blank_lines(s)
+
 #----------------------------------------------------------------------------------------------
 
 print("\nCreating database...")
@@ -89,51 +108,87 @@ if bigstore_ram_size_bytes is None:
 
 shards_placement = 'sparse' if args.sparse else 'dense'
 
-fields_t = r'''
-"name": "{{args.name}}",
-"port": 12000,
-"memory_size": {{mem_bytes}},
-"sharding": {{sharding}},
-"shards_count" : {{shards}},
-"shard_key_regex": [ { "regex": ".*\\{(?<tag>.*)\\}.*" }, { "regex": "(?<tag>.*)" } ],
-"replication": {{replication}},
-"shards_placement": "{{shards_placement}}",
-"proxy_policy": "all-nodes",
-"hash_slots_policy": "16k"
+v1_fields_t = r'''
+name: {{args.name}}
+port: 12000
+memory_size: {{mem_bytes}}
+sharding: {{sharding}}
+shards_count : {{shards}}
+shard_key_regex: 
+  - regex: ".*\\{(?<tag>.*)\\}.*"
+  - regex: "(?<tag>.*)"
+replication: {{replication}}
+shards_placement: {{shards_placement}}
+proxy_policy: all-nodes
+# data_persistence: aof
+# aof_policy: appendfsync-always
+hash_slots_policy: 16k
+{{modules_defs}}
+{{flash_defs}}
 '''
 
-fields_flash_t = r'''
-,
-"bigstore": {{bigstore}},
-"bigstore_ram_size": {{bigstore_ram_size_bytes}}
+v2_fields_t = r'''
+bdb:
+  name: {{args.name}}
+  port: 12000
+  memory_size: {{mem_bytes}}
+  sharding: {{sharding}}
+  shards_count: {{shards}}
+  shard_key_regex:
+    - regex: ".*\\{(?<tag>.*)\\}.*"
+    - regex: "(?<tag>.*)"
+  replication: {{replication}}
+  shards_placement: {{shards_placement}}
+  proxy_policy: all-nodes
+  data_persistence: aof
+# data_persistence: snapshot
+  aof_policy: appendfsync-always
+  hash_slots_policy: 16k
+  {{modules_defs_yml}}
+  {{flash_defs}}
+# {{recovery_plan}}
 '''
-
-if flash:
-    fields_t += fields_flash_t
 
 # "port": 16379,
 # "oss_cluster": false,
 # "proxy_policy": "single",
-# "data_persistence": "aof",
-# "aof_policy": "appendfsync-always",
 
-fields = jinja2.Template(fields_t).render(locals())
+fields_flash_t = r'''
+bigstore: {{bigstore}}
+bigstore_ram_size: {{bigstore_ram_size_bytes}}
+'''
 
-q=''
+recovery_plan_t = r'''
+recovery_plan:
+  data_files:
+    - shard_slots: 0-16383
+      node_uid: 1
+      filename: /opt/view/rlec/in/db1-1-1.aof
+'''
+
+if rlec_version < paella.Version('6.0.8'):
+    api_ver = "v1"
+    fields_t = v1_fields_t
+else:
+    api_ver = "v2"
+    fields_t = v2_fields_t
+
 if not args.no_modules and os.path.exists('/opt/view/rlec/modules.json'):
     mod_list = paella.fread('/opt/view/rlec/modules.json')
+    modules_defs = 'module_list: [%s]' % mod_list
 else:
-    mod_list = ''
-if mod_list.strip() != '':
-    q = '{ %s, "module_list": [%s] }' % (fields, mod_list)
-else:
-    q = "{ %s }" % (fields)
+    modules_defs = ''
+
+flash_defs = meld(fields_flash_t) if flash else ''
+recovery_plan = meld(recovery_plan_t)
+
+q = json.dumps(yaml.safe_load(meld(fields_t)))
 
 paella.fwrite('/opt/view/rlec/create-db.rest', q)
 
 with no_ssl_verification():
-    res = requests.post('https://localhost:9443/v1/bdbs', data=q, headers={'Content-Type': 'application/json'}, auth=('a@a.com', 'a'))
-# res = subprocess.check_output(['curl', '-s', '-k', '-u', 'a@a.com:a', '-X', 'POST', 'https://localhost:9443/v1/bdbs', '-H', 'Content-type: application/json', '-d', '@/opt/view/rlec/create-db.rest'])
+    res = requests.post('https://localhost:9443/{api_ver}/bdbs'.format(api_ver=api_ver),
+                        data=q, headers={'Content-Type': 'application/json'}, auth=('a@a.com', 'a'))
 resj = json.loads(res.content)
 paella.fwrite('/opt/view/rlec/db.json', json.dumps(resj, indent=4))
 
